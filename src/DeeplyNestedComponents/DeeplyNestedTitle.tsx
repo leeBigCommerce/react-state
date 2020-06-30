eb8cd0500ed3814ee741ebdec255acf605dc0541
Lee Dunkley <lee.dunkley@bigcommerce.com>
2020-06-30 17:51:41 +1000

    fix out of sync wording

--- src/DeeplyNestedComponents/DeeplyNestedTitle.tsx
+++ src/DeeplyNestedComponents/DeeplyNestedTitle.tsx
@@ -5,5 +5,5 @@ import { buttonClickedContext } from './buttonClickedContext';
 export const DeeplyNestedTitle: React.FC = () => {
     const { buttonClicked } = React.useContext(buttonClickedContext);
 
-    return <h1>{buttonClicked ? 'Thank you' : 'Please Click Me'}</h1>;
+    return <h1>{buttonClicked ? 'Thank you' : 'Please click the button'}</h1>;
 };
