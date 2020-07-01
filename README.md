233a8aa21dabf14a7025e8d37487e0522ec5c5b6
Lee Dunkley <lee.dunkley@bigcommerce.com>
2020-07-01 11:39:43 +1000

    move explanation and comments to readme

--- README.md
+++ README.md
@@ -1,12 +1,113 @@
 # The Gist
 
 This repo is an exploration of the different state solutions in a React app.
-The solutions are portrayed in descending order of preference within `src/App`.
 
-This order of preference was come to by:
+Below is a set of ever more complicated state related problems paired with example solutions and commentary.
 
-1. Aiming for the simplest pattern to solve the problem at hand (lowest cyclomatic complexity)
-2. Aiming for as few technologies and libraries as possible (lowest bar to entry for developers)
+Solutions have been selected on the basis of:
+
+1. Being the simplest pattern to solve the problem at hand (lowest cyclomatic complexity)
+2. Using as few technologies and libraries as possible (lowest bar to entry for developers)
+3. Isolation of concerns
+
+---
+
+## Problem One
+
+### Requirements
+
+1. My component needs to keep track of a value
+
+### Solution
+
+-   Use `useState` in the _lowest possible_ scope of your component tree
+
+### Example
+
+`src/SingleComponentState`
+
+### Comments
+
+-   Hooks over classes is a debate for another time
+-   Invoking `useState` in the _lowest possible_ scope helps towards a separation of concerns
+
+--
+
+## Problem Two
+
+### Requirements
+
+1. My component needs to keep track of a value
+2. Some other components also need that value
+
+### Solution
+
+-   Use `useState` in the _lowest possible_ scope of your component tree
+-   Pass that value to child components as props
+
+### Example
+
+`src/OneLevelOfNesting`
+
+### Comments
+
+-   Your child components will re-render with any change to that state value from the parent.
+-   Your child components can be written to simple functional components, agnostic to the source of their prop values.
+
+--
+
+## Problem Three
+
+### Requirements
+
+1. My component needs to keep track of a value
+2. Some other components also need that value
+3. Those other components are nested within further components which _do not_ require that value
+
+### Solution
+
+-   Use `useState` in the _lowest possible_ scope of your component tree
+-   Consider re-organizing your code so that components that are concerned with the same values are in the same area
+-   Make use of the `children` pattern to flatten your component tree
+-   Pass that value to child components as props
+
+### Example
+
+`src/FlattenedComponentTree`
+
+### Comments
+
+-   'Prop Drilling' is an anti-pattern sometimes used to solve this problem. However, it wrongly forces intermediatory components to require values _only_ to pass on them down the tree. Components should only require values (through props) which directly work towards them realising their specific use case.
+-   The existence of multiple components, each needing the same value(s), but being in wildly different positions in the component tree is a code smell. It points to either a repo architectural issue or a UX issue that could benefit from refactoring.
+
+--
+
+## Problem Four
+
+### Requirements
+
+1. My component needs to keep track of a value
+2. Some other components also need that value
+3. Those other components are nested within further components which _do not_ require that value
+4. Re-organising and/or flattening components has reached some kind of limit
+
+### Solution
+
+-   Use `useState`
+-   Pass that state into `createContext` to create a `context` and a `Provider` for that value
+-   Use the `Provider` in the _lowest possible_ scope of your component tree
+-   Consume the `context` (with `useContext`) within any components that need that value
+
+### Example
+
+`src/DeeplyNestedComponents`
+
+### Comments
+
+-   `context` is React's solution passing things around without using props
+-   As we're now chaining a few different techniques together (`useState`, `createContext`, `useContext`) to achieve a singular aim, it's good practice to hide these away as implementation details behind a more concise API (as done in our example).
+
+--
 
 ## Available Scripts
 
--- src/App.tsx
+++ src/App.tsx
@@ -6,112 +6,30 @@ import { Example as FlattenedComponentTree } from './FlattenedComponentTree';
 import { Example as DeeplyNestedComponents } from './DeeplyNestedComponents';
 
 export const App: React.FC = () => (
-    <>
-        <section>
-            <h1>State Patterns in Descending Order of Preference</h1>
-            <h2>1. For basic component memory:</h2>
-            <ul>
-                <li>
-                    use <code>useState</code>
-                </li>
-            </ul>
-            <h3>Example:</h3>
-            <SingleComponentState />
-            <p>
-                (see &quot;<code>./src/SingleComponentState</code>&quot; for code)
-            </p>
-            <hr />
-            <h2>2. When multiple components require that state:</h2>
-            <ul>
-                <li>Place that state in the LOWEST possible scope</li>
-                <li>Pass the value in as a prop</li>
-            </ul>
-            <h3>Example:</h3>
-            <OneLevelOfNesting />
-            <p>
-                (see &quot;<code>./src/OneLevelOfNesting</code>&quot; for code)
-            </p>
-            <hr />
-            <h2>3. When multiple components require that state, but are deeply nested:</h2>
-            <ul>
-                <li>
-                    Consider re-organizing your code so that components that are concerned with the same values are in
-                    the same area
-                </li>
-                <li>
-                    Make use of the <code>children</code> pattern and try flattening your component tree
-                </li>
-                <li>Place that state in the LOWEST possible scope</li>
-                <li>Then pass the value in as a prop</li>
-            </ul>
-            <h3>Example:</h3>
-            <FlattenedComponentTree />
-            <p>
-                (see &quot;<code>./src/FlattenedComponentTree</code>&quot; for code)
-            </p>
-            <hr />
-            <h2>
-                4. When multiple components require that state, are deeply nested and you&apos;ve somehow reached the
-                limit of re-organizing and flattening:
-            </h2>
-            <ul>
-                <li>
-                    Make use of <code>context</code>
-                </li>
-            </ul>
-            <h3>Example:</h3>
-            <DeeplyNestedComponents />
-            <p>
-                (see &quot;<code>./src/DeeplyNestedComponents</code>&quot; for code)
-            </p>
-            <hr />
-        </section>
-        <section>
-            <h1>&quot;Wait, what if...&quot;</h1>
-            <ol>
-                <li>
-                    &quot;I&apos;ve used pattern 4 and have ended up with a bunch of context providers on top of each
-                    other?&quot;
-                    <ul>
-                        <li>Are you sure you&apos;ve got all the providers in their lowest possible scopes?</li>
-                        <li>
-                            If you are, yet you&apos;ve been forced to add a provider very high in your app, it could be
-                            a smell that the repo or the UX needs re-organizing to keep things with the similar concerns
-                            in a similar area.
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    &quot;The state I&apos;m storing has deeply interrelated attributes; when X value changes within it,
-                    Y and Z have to change too&quot;
-                    <ul>
-                        <li>
-                            Sounds like business logic creeping into the view layer. If an Android App and an iPhone App
-                            also wanted to portray this view to users, would they need their own implementation of the
-                            exact same logic? If so, this should probably be pushed up a layer to your source of truth
-                            (e.g. the API providing and receiving changes to this entity).
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    &quot;I tried that, but I couldn&apos;t convince the Backend Developers / my app is the highest
-                    source of truth for that entity&quot;
-                    <ul>
-                        <li>
-                            Create the API you wish you had by creating a custom hook which (potentially) uses
-                            useReducer under the hood OR consider using Redux with a store specific to that entity.
-                        </li>
-                        <li>
-                            Remember to aim for as much decoupling as possible between your chosen solution and your
-                            visual components.
-                        </li>
-                        <li>
-                            You might want to consider persisting this complex state somewhere - what if your user
-                            refreshes?
-                        </li>
-                    </ul>
-                </li>
-            </ol>
-        </section>
-    </>
+    <section>
+        <h1>Problem 1 Example</h1>
+        <SingleComponentState />
+        <p>
+            (see &quot;<code>./src/SingleComponentState</code>&quot; for code)
+        </p>
+        <hr />
+        <h1>Problem 2 Example</h1>
+        <OneLevelOfNesting />
+        <p>
+            (see &quot;<code>./src/OneLevelOfNesting</code>&quot; for code)
+        </p>
+        <hr />
+        <h1>Problem 3 Example</h1>
+        <FlattenedComponentTree />
+        <p>
+            (see &quot;<code>./src/FlattenedComponentTree</code>&quot; for code)
+        </p>
+        <hr />
+        <h1>Problem 4 Example</h1>
+        <DeeplyNestedComponents />
+        <p>
+            (see &quot;<code>./src/DeeplyNestedComponents</code>&quot; for code)
+        </p>
+        <hr />
+    </section>
 );
